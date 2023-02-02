71200cbff9fa1ddca4336d000dde650c0115b5a2
marwan <marwankefah@gmail.com>
2023-02-02 15:23:44 +0800

    edited logging info

--- core/trainers/densenet121_trainer.py
+++ core/trainers/densenet121_trainer.py
@@ -98,4 +98,4 @@ class DenseNet121Trainer:
         torch.save(self.model.state_dict(), self.local_model_path
                    + '/local_model_' + str(self.id) + '.pt')
 
-        logger.info("saved local model of Client: " + str(self.id))
+        # logger.info("saved local model of Client: " + str(self.id))
