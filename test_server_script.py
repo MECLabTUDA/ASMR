eba1b39a0b03e84c34a15b08ed0687d07514a7dc
marwan <marwankefah@gmail.com>
2023-02-04 12:06:21 +0800

    added server testing script

--- test_server_script.py
+++ test_server_script.py
@@ -26,14 +26,29 @@ clients_init, clients_info = retrieve_clients(client_cfg)
 
 server = Server(server_cfg, [])
 
-global_weight = server.model.state_dict()
+global_weight = copy.deepcopy(server.model.state_dict())
+global_weight_2 = server.model.state_dict()
 
 # server.evaluate(global_weight)
+zero_state_dict = OrderedDict()
+for key, value in global_weight.items():
+    zero_state_dict[key] = torch.zeros_like(value).float()
 
-logger.info('First evaluation fiinished')
-server.evaluate(torch.zeros_like(global_weight))
-logger.info('zeros evaluation fiinished')
-server.evaluate(torch.ones_like(global_weight))
-logger.info('ones evaluation fiinished')
+logger.info('First evaluation Zeros')
+server.evaluate(zero_state_dict)
+
+ones_state_dict = OrderedDict()
+for key, value in global_weight.items():
+    ones_state_dict[key] = torch.ones_like(value).float()
+
+logger.info('Second evaluation Ones')
+server.evaluate(ones_state_dict)
+
+
+logger.info('Third evaluation no deep')
+server.evaluate(global_weight_2)
+
+logger.info('fourth evaluation copy deep')
+server.evaluate(global_weight)
 
 
