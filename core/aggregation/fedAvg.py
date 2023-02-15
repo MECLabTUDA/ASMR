10ee6fd07bf8cded7df3162b284dd5227e00f00e
mirko-code <mirko.konstantin@gmail.com>
2023-02-15 22:48:39 +0100

    fixed dict issues in fedavg

--- core/aggregation/fedAvg.py
+++ core/aggregation/fedAvg.py
@@ -8,19 +8,18 @@ class FedAvg:
         self.clients_info = clients_info
         self.global_model_path = global_model_path
         self.total_samples = 0
-        for client_dict in self.clients_info:
-            if client_dict['active']:
-                self.total_samples += client_dict['num_samples']
+        for client_id in self.clients_info:
+            if self.clients_info[client_id]['active']:
+                self.total_samples += self.clients_info[client_id]['num_samples']
 
     def get_info(self):
         print("this is a FedAvg")
 
     def _average_weights(self):
-        
 
-        client_sd = [c['weights'] for c in self.clients_info if c['active']]
-        cw = [c['num_samples'] / self.total_samples for c in self.clients_info if c['active']]
-        #ssd = copy.deepcopy(self.clients_info[0]['weights'])
+        client_sd = [self.clients_info[c]['weights'] for c in self.clients_info if self.clients_info[c]['active']]
+        cw = [self.clients_info[c]['num_samples'] / self.total_samples for c in self.clients_info if self.clients_info[c]['active']]
+        # ssd = copy.deepcopy(self.clients_info[0]['weights'])
         ssd = self._create_zero_state_dict(client_sd[0])
         ''' 
         for client in self.clients_info:
