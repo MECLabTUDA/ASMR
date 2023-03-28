79a2d18698c3cb738074fc78254f5fbb364ec967
mirko-code <mirko.konstantin@gmail.com>
2023-03-28 16:08:02 +0000

    configs with better Attack performances + requirements file

--- attack_exps.sh
+++ attack_exps.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-python3 main.py --exp_path=camelyon17_none_fedavgm.yml --logfile=camelyon_none_avgm.log --gpu=2,3
-python3 main.py --exp_path=camelyon17_none_fedavg.yml --logfile=camelyon_none_avg.log --gpu=2,3
+#python3 main.py --exp_path=camelyon17_none_fedavgm.yml --logfile=camelyon_none_avgm.log --gpu=2,3
+#python3 main.py --exp_path=camelyon17_none_fedavg.yml --logfile=camelyon_none_avg.log --gpu=2,3
 
-python3 main.py --exp_path=camelyon17_ana_fedavgm.yml --logfile=camelyon_ana_avgm.log --gpu=2,3
-python3 main.py --exp_path=camelyon17_ana_fedavg.yml --logfile=camelyon_ana_avg.log --gpu=2,3
+python3 main.py --exp_path=camelyon17_ana_fedavgm.yml --logfile=camelyon_ana_avgm_new.log --gpu=2,3,4
+python3 main.py --exp_path=camelyon17_ana_fedavg.yml --logfile=camelyon_ana_avg_new.log --gpu=2,3,4
 
-python3 main.py --exp_path=camelyon17_sfa_fedavgm.yml --logfile=camelyon_sfa_avgm.log --gpu=2,3
-python3 main.py --exp_path=camelyon17_sfa_fedavg.yml --logfile=camelyon_sfa_avg.log --gpu=2,3
+python3 main.py --exp_path=camelyon17_sfa_fedavgm.yml --logfile=camelyon_sfa_avgm_new.log --gpu=2,3,4
+python3 main.py --exp_path=camelyon17_sfa_fedavg.yml --logfile=camelyon_sfa_avg_new.log --gpu=2,3,4
 
