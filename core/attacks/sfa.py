5564e8b6a9b4a5286b77b5bb55811bbe355fba36
mirko-code <mirko.konstantin@gmail.com>
2023-04-05 15:55:14 +0200

    add default value for flig_signs

--- core/attacks/sfa.py
+++ core/attacks/sfa.py
@@ -1,7 +1,7 @@
 import copy
 
 
-def flip_signs(w, scale):
+def flip_signs(w, scale=-1):
     w_attacked = copy.deepcopy(w)
     for k in w_attacked.keys():
         w_attacked[k] = scale * w_attacked[k].float()
