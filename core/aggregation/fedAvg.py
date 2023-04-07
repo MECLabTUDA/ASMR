78b08c5e88a76e1ca4a78a0a4e7e66b2617d67a3
mirko-code <mirko.konstantin@gmail.com>
2023-04-07 19:42:46 +0200

    enables GPU usage for aggregation

--- core/aggregation/fedAvg.py
+++ core/aggregation/fedAvg.py
@@ -4,7 +4,8 @@ import copy
 
 
 class FedAvg:
-    def __init__(self, clients_info, global_model_path):
+    def __init__(self, clients_info, global_model_path, device='cuda:0'):
+        self.device = device
         self.clients_info = clients_info
         self.global_model_path = global_model_path
         self.total_samples = 0
@@ -12,14 +13,17 @@ class FedAvg:
         for client_id in self.clients_info:
             if self.clients_info[client_id]['active']:
                 self.total_samples += self.clients_info[client_id]['num_samples']
+                self.clients_info[client_id]['weights'] = {k: v.to(self.device) for k, v in self.clients_info[client_id]['weights'].items()}
 
     def get_info(self):
         print("this is a FedAvg")
 
     def _average_weights(self):
 
-        client_sd = [self.clients_info[c]['weights'] for c in self.clients_info if self.clients_info[c]['active'] and self.clients_info[c]['weights']!=None]
-        cw = [self.clients_info[c]['num_samples'] / self.total_samples for c in self.clients_info if self.clients_info[c]['active']]
+        client_sd = [self.clients_info[c]['weights'] for c in self.clients_info if
+                     self.clients_info[c]['active'] and self.clients_info[c]['weights'] is not None]
+        cw = [self.clients_info[c]['num_samples'] / self.total_samples for c in self.clients_info if
+              self.clients_info[c]['active']]
         # ssd = copy.deepcopy(self.clients_info[0]['weights'])
         ssd = self._create_zero_state_dict(client_sd[0])
         ''' 
@@ -37,7 +41,7 @@ class FedAvg:
     def _create_zero_state_dict(self, state_dict):
         zero_state_dict = OrderedDict()
         for key, value in state_dict.items():
-            zero_state_dict[key] = torch.zeros_like(value).float()
+            zero_state_dict[key] = torch.zeros_like(value).float().to(self.device)
         return zero_state_dict
 
     def _save_agg_model(self, agg_state_dict):
@@ -46,5 +50,6 @@ class FedAvg:
     def aggregate(self, clients_info):
         self.clients_info = clients_info
         agg_state_dict = self._average_weights()
+        agg_state_dict = {k: v.cpu() for k, v in agg_state_dict.items()}
         self._save_agg_model(agg_state_dict)
         return agg_state_dict
