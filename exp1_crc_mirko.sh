30ed8cf6994f7728fdd910b8cb02ca041bb72a1f
mirko-code <mirko.konstantin@gmail.com>
2023-05-10 17:39:44 +0000

    changing in config files

--- exp1_crc_mirko.sh
+++ exp1_crc_mirko.sh
@@ -4,7 +4,7 @@
 #SBATCH -J CRC_mirko
 #SBATCH -n 20
 #SBATCH -c 8
-#SBATCH --mem-per-cpu=4000
+#SBATCH --mem-per-cpu=8000
 #SBATCH --gres=gpu:4
 #SBATCH -t 40:00:10
 
@@ -12,25 +12,25 @@
 #SBATCH -e /gris/gris-f/homestud/mikonsta/master-thesis/FedPath/errlog/%j_%J.err
 
 ## CRC - ANA - Mirko
-python3 main.py --gpu=0,1,2,3 --exp_path=experiment1/crc/mirko/crc_ana_fedavgm_seed1.yml --logfile=final/exp1/mirko/crc/ana/crc_fedavgm_seed1.log
-python3 main.py --gpu=0,1,2,3 --exp_path=experiment1/crc/mirko/crc_ana_fedavgm_seed2.yml --logfile=final/exp1/mirko/crc/ana/crc_fedavgm_seed2.log
-python3 main.py --gpu=0,1,2,3 --exp_path=experiment1/crc/mirko/crc_ana_fedavgm_seed3.yml --logfile=final/exp1/mirko/crc/ana/crc_fedavgm_seed3.log
-python3 main.py --gpu=0,1,2,3 --exp_path=experiment1/crc/mirko/crc_ana_fedavgm_seed4.yml --logfile=final/exp1/mirko/crc/ana/crc_fedavgm_seed4.log
-python3 main.py --gpu=0,1,2,3 --exp_path=experiment1/crc/mirko/crc_ana_fedavgm_seed5.yml --logfile=final/exp1/mirko/crc/ana/crc_fedavgm_seed5.log
-python3 main.py --gpu=0,1,2,3 --exp_path=experiment1/crc/mirko/crc_ana_fedavgm_seed6.yml --logfile=final/exp1/mirko/crc/ana/crc_fedavgm_seed6.log
-python3 main.py --gpu=0,1,2,3 --exp_path=experiment1/crc/mirko/crc_ana_fedavgm_seed7.yml --logfile=final/exp1/mirko/crc/ana/crc_fedavgm_seed7.log
-python3 main.py --gpu=0,1,2,3 --exp_path=experiment1/crc/mirko/crc_ana_fedavgm_seed8.yml --logfile=final/exp1/mirko/crc/ana/crc_fedavgm_seed8.log
-python3 main.py --gpu=0,1,2,3 --exp_path=experiment1/crc/mirko/crc_ana_fedavgm_seed9.yml --logfile=final/exp1/mirko/crc/ana/crc_fedavgm_seed9.log
-python3 main.py --gpu=0,1,2,3 --exp_path=experiment1/crc/mirko/crc_ana_fedavgm_seed10.yml --logfile=final/exp1/mirko/crc/ana/crc_fedavgm_seed10.log
+#python3 main.py --gpu=0,1,2,3 --exp_path=experiment1/crc/mirko/crc_ana_fedavgm_seed1.yml --logfile=final/exp1/mirko/crc/ana/crc_fedavgm_seed1.log
+#python3 main.py --gpu=0,1,2,3 --exp_path=experiment1/crc/mirko/crc_ana_fedavgm_seed2.yml --logfile=final/exp1/mirko/crc/ana/crc_fedavgm_seed2.log
+#python3 main.py --gpu=0,1,2,3 --exp_path=experiment1/crc/mirko/crc_ana_fedavgm_seed3.yml --logfile=final/exp1/mirko/crc/ana/crc_fedavgm_seed3.log
+#python3 main.py --gpu=0,1,2,3 --exp_path=experiment1/crc/mirko/crc_ana_fedavgm_seed4.yml --logfile=final/exp1/mirko/crc/ana/crc_fedavgm_seed4.log
+#python3 main.py --gpu=0,1,2,3 --exp_path=experiment1/crc/mirko/crc_ana_fedavgm_seed5.yml --logfile=final/exp1/mirko/crc/ana/crc_fedavgm_seed5.log
+#python3 main.py --gpu=0,1,2,3 --exp_path=experiment1/crc/mirko/crc_ana_fedavgm_seed6.yml --logfile=final/exp1/mirko/crc/ana/crc_fedavgm_seed6.log
+#python3 main.py --gpu=0,1,2,3 --exp_path=experiment1/crc/mirko/crc_ana_fedavgm_seed7.yml --logfile=final/exp1/mirko/crc/ana/crc_fedavgm_seed7.log
+#python3 main.py --gpu=0,1,2,3 --exp_path=experiment1/crc/mirko/crc_ana_fedavgm_seed8.yml --logfile=final/exp1/mirko/crc/ana/crc_fedavgm_seed8.log
+#python3 main.py --gpu=0,1,2,3 --exp_path=experiment1/crc/mirko/crc_ana_fedavgm_seed9.yml --logfile=final/exp1/mirko/crc/ana/crc_fedavgm_seed9.log
+#python3 main.py --gpu=0,1,2,3 --exp_path=experiment1/crc/mirko/crc_ana_fedavgm_seed10.yml --logfile=final/exp1/mirko/crc/ana/crc_fedavgm_seed10.log
 
 ## CRC - SFA - Mirko
 python3 main.py --gpu=0,1,2,3 --exp_path=experiment1/crc/mirko/crc_sfa_fedavgm_seed1.yml --logfile=final/exp1/mirko/crc/sfa/crc_fedavgm_seed1.log
-python3 main.py --gpu=0,1,2,3 --exp_path=experiment1/crc/mirko/crc_sfa_fedavgm_seed2.yml --logfile=final/exp1/mirko/crc/sfa/crc_fedavgm_seed2.log
-python3 main.py --gpu=0,1,2,3 --exp_path=experiment1/crc/mirko/crc_sfa_fedavgm_seed3.yml --logfile=final/exp1/mirko/crc/sfa/crc_fedavgm_seed3.log
-python3 main.py --gpu=0,1,2,3 --exp_path=experiment1/crc/mirko/crc_sfa_fedavgm_seed4.yml --logfile=final/exp1/mirko/crc/sfa/crc_fedavgm_seed4.log
-python3 main.py --gpu=0,1,2,3 --exp_path=experiment1/crc/mirko/crc_sfa_fedavgm_seed5.yml --logfile=final/exp1/mirko/crc/sfa/crc_fedavgm_seed5.log
-python3 main.py --gpu=0,1,2,3 --exp_path=experiment1/crc/mirko/crc_sfa_fedavgm_seed6.yml --logfile=final/exp1/mirko/crc/sfa/crc_fedavgm_seed6.log
-python3 main.py --gpu=0,1,2,3 --exp_path=experiment1/crc/mirko/crc_sfa_fedavgm_seed7.yml --logfile=final/exp1/mirko/crc/sfa/crc_fedavgm_seed7.log
-python3 main.py --gpu=0,1,2,3 --exp_path=experiment1/crc/mirko/crc_sfa_fedavgm_seed8.yml --logfile=final/exp1/mirko/crc/sfa/crc_fedavgm_seed8.log
-python3 main.py --gpu=0,1,2,3 --exp_path=experiment1/crc/mirko/crc_sfa_fedavgm_seed9.yml --logfile=final/exp1/mirko/crc/sfa/crc_fedavgm_seed9.log
-python3 main.py --gpu=0,1,2,3 --exp_path=experiment1/crc/mirko/crc_sfa_fedavgm_seed10.yml --logfile=final/exp1/mirko/crc/sfa/crc_fedavgm_seed10.log
+#python3 main.py --gpu=0,1,2,3 --exp_path=experiment1/crc/mirko/crc_sfa_fedavgm_seed2.yml --logfile=final/exp1/mirko/crc/sfa/crc_fedavgm_seed2.log
+#python3 main.py --gpu=0,1,2,3 --exp_path=experiment1/crc/mirko/crc_sfa_fedavgm_seed3.yml --logfile=final/exp1/mirko/crc/sfa/crc_fedavgm_seed3.log
+#python3 main.py --gpu=0,1,2,3 --exp_path=experiment1/crc/mirko/crc_sfa_fedavgm_seed4.yml --logfile=final/exp1/mirko/crc/sfa/crc_fedavgm_seed4.log
+#python3 main.py --gpu=0,1,2,3 --exp_path=experiment1/crc/mirko/crc_sfa_fedavgm_seed5.yml --logfile=final/exp1/mirko/crc/sfa/crc_fedavgm_seed5.log
+#python3 main.py --gpu=0,1,2,3 --exp_path=experiment1/crc/mirko/crc_sfa_fedavgm_seed6.yml --logfile=final/exp1/mirko/crc/sfa/crc_fedavgm_seed6.log
+#python3 main.py --gpu=0,1,2,3 --exp_path=experiment1/crc/mirko/crc_sfa_fedavgm_seed7.yml --logfile=final/exp1/mirko/crc/sfa/crc_fedavgm_seed7.log
+#python3 main.py --gpu=0,1,2,3 --exp_path=experiment1/crc/mirko/crc_sfa_fedavgm_seed8.yml --logfile=final/exp1/mirko/crc/sfa/crc_fedavgm_seed8.log
+#python3 main.py --gpu=0,1,2,3 --exp_path=experiment1/crc/mirko/crc_sfa_fedavgm_seed9.yml --logfile=final/exp1/mirko/crc/sfa/crc_fedavgm_seed9.log
+#python3 main.py --gpu=0,1,2,3 --exp_path=experiment1/crc/mirko/crc_sfa_fedavgm_seed10.yml --logfile=final/exp1/mirko/crc/sfa/crc_fedavgm_seed10.log
